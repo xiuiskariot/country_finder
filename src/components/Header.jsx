b21868d65bee78c4d57a628c862cdf8554be8ebb
Ksusha <xiuschv@yandex.ru>
2023-07-08 17:50:33 +0300

    initial

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import { ContainerS } from "./Container";
+import { IoMoon, IoMoonOutline } from "react-icons/io5";
+
+const HeaderS = styled.header`
+  box-shadow: var(--shadow);
+  background-color: var(--colors-ui-base);
+`;
+const WrapperS = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
+const TitleS = styled.a`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  text-decoration: none;
+  font-weight: var(--fw-bold);
+`;
+const ModeSwitcherS = styled.div`
+  color: var(--colors-text);
+  font-size: var(--fs-sm);
+  cursor: pointer;
+  display: flex;
+  flex-direction: base-line
+`;
+
+export const Header = () => {
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => setTheme(theme === "light" ? "dark" : "light");
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
+  return (
+    <HeaderS>
+      <ContainerS>
+        <WrapperS>
+          <TitleS>Where is the world?</TitleS>
+          <ModeSwitcherS onClick={toggleTheme}>
+            {theme === "light" ? (
+              <IoMoonOutline size="14px" />
+            ) : (
+              <IoMoon size="14px" />
+            )}
+            <span style = {{marginLeft: "0.75rem"}}>{theme}</span>
+          </ModeSwitcherS>
+        </WrapperS>
+      </ContainerS>
+    </HeaderS>
+  );
+};
